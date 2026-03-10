a893d209dbcdf3c1fdd004cefb025663ad186aad
red arch <abhishekkaush39@gmail.com>
2026-03-10 22:58:05 +0530

    update ui

--- comp/Nav.jsx
+++ comp/Nav.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Important from '../pages/important';
 // import './Nav.css';
 const Nav = () => {
-  const navcontent = ['Dashboard', 'Important', 'Completed', 'Do It now'];
+  const router = createBrowserRouter([
+    {
+      path: '/complete',
+      element: <Important />,
+    },
+  ]);
+  const navcontent = ['dashboard', 'important', 'completed', 'do it now'];
   return (
-    <div className='firstDiv min-w-48 flex justify-between items-center flex-col'>
-      <h1 className='text-black flex justify-center my-3 text-5xl font-semibold'>
+    <div className='firstDiv min-w-48 flex justify-between items-center flex-col bg-[#161B22] text-white'>
+      <h1 className='text-white flex justify-center my-3 text-5xl font-semibold'>
         TM
       </h1>
       <div className='task-nav-left'>
         {navcontent.map((content, index) => (
-          <li className='capitalize list-none p-2' key={index}>
+          <li
+            id={content.trim().replace(/\s+/g, '-')}
+            className='cursor-pointer capitalize list-none p-2'
+            key={index}
+          >
             {content}
           </li>
         ))}
       </div>
+      <RouterProvider router={router} />
       <div className='h-10'></div>
     </div>
   );
---
+++ pages/important/Important.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const important = () => {
+  return <div>hey i'm important</div>;
+};
+
+export default important;
