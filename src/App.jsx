89a80295570effcf55defe5d1702e8a536883cef
red arch <abhishekkaush39@gmail.com>
2026-03-19 22:09:09 +0530

    Fix case sensitivity: rename Complete.jsx to complete.jsx and update import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Nav from '../comp/Nav';
 import Taskarea from '../comp/task-area/Taskarea';
 import { Routes, Route } from 'react-router-dom';
 import Important from '../pages/important/Important';
-import Complete from '../pages/complete/Complete.jsx';
+import Complete from '../pages/complete/complete.jsx';
 import Do_it_now from '../pages/doitnow/Do_it_now';
 
 function App() {
